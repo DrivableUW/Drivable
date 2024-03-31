a0ff94368f83654a98a74b1abe00c93122a465be
Krishan-Patel <61567619+Krishan-Patel@users.noreply.github.com>
2024-03-31 13:15:31 -0400

    updated aggregateDriveData and InsuranceExport code to be OOP

--- app/src/main/java/com/cs446g15/app/util/InsuranceExport.kt
+++ app/src/main/java/com/cs446g15/app/util/InsuranceExport.kt
@@ -6,7 +6,7 @@ import androidx.core.content.FileProvider
 import com.cs446g15.app.MainActivity
 import com.cs446g15.app.data.AggregatedDriveData
 import com.cs446g15.app.data.Drive
-import com.cs446g15.app.data.aggregateDriveData
+import com.cs446g15.app.data.AggregateDriveData
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.File
@@ -18,31 +18,40 @@ data class ExportedDriveData(
     val aggregatedData: AggregatedDriveData
 )
 
-suspend fun exportDrivesWithAggregation(drives: List<Drive>) {
-    val json = Json { prettyPrint = true }
+class InsuranceExport {
+    companion object {
+        suspend fun exportDrivesWithAggregation(drives: List<Drive>) {
+            val json = Json { prettyPrint = true }
 
-    val aggregatedData = aggregateDriveData(drives)
+            val aggregatedData = AggregateDriveData.computeAggregateDriveData(drives)
 
-    val exportedData = ExportedDriveData(drives, aggregatedData)
+            val exportedData = ExportedDriveData(drives, aggregatedData)
 
-    val jsonExportedData = json.encodeToString(ExportedDriveData.serializer(), exportedData)
+            val jsonExportedData = json.encodeToString(ExportedDriveData.serializer(), exportedData)
 
-    val attested = IntegrityProvider.attest(jsonExportedData)
+            val attested = IntegrityProvider.attest(jsonExportedData)
 
-    val fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + "_drive_export.jwt"
-    val file = File(MainActivity.appContext.cacheDir, fileName)
-    file.writeText(attested)
+            val fileName = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + "_drive_export.jwt"
+            val file = File(MainActivity.appContext.cacheDir, fileName)
+            file.writeText(attested)
 
-    val uri = FileProvider.getUriForFile(MainActivity.appContext, "com.cs446g15.app.fileprovider", file)
+            val uri = FileProvider.getUriForFile(
+                MainActivity.appContext,
+                "com.cs446g15.app.fileprovider",
+                file
+            )
 
-    val shareIntent = Intent().apply {
-        action = Intent.ACTION_SEND
-        type = "application/jwt"
-        putExtra(Intent.EXTRA_STREAM, uri)
-        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "application/jwt"
+                putExtra(Intent.EXTRA_STREAM, uri)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+
+            val chooserIntent = Intent.createChooser(shareIntent, "Export Drive Data")
+            chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            MainActivity.appContext.startActivity(chooserIntent)
+        }
     }
-
-    val chooserIntent = Intent.createChooser(shareIntent, "Export Drive Data")
-    chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    MainActivity.appContext.startActivity(chooserIntent)
 }
